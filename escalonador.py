13252209ad7d272806675888b43df3eb7918bf4d
Myke Amorim <myke.amorim@usp.br>
2023-10-21 18:00:52 -0300

    Implementadas instruções de E/S e Saída

--- escalonador.py
+++ escalonador.py
@@ -1,133 +1,87 @@
 from BCP import BCP
-        
-        
+
+# Inicialização das listas
 prontos = []
-
-block = []
-
+bloqueados = []
 tabela_de_processos = []
 
+# Classe BCP (Blocos de Controle de Processo) continua a mesma
 
+# Classe Escalonador
+def log(message):
+    with open("logXX.txt", "a") as log_file:
+        log_file.write(message + "\n")
 
 class Escalonador:
-    def __init__(self, tabela_de_processos, pronto, bloqueado):
+    def __init__(self, tabela_de_processos, pronto, bloqueado, quantum):
         self.tabela_de_processos = tabela_de_processos
         self.lista_pronto = pronto
         self.lista_bloqueado = bloqueado
-        self.quantum = 3
-        
+        self.quantum = quantum
+
     def decrementa_espera_bloqueados(self):
-        i = 0
-        processos_liberados = []
-        while i < len(self.lista_bloqueado):  # decremento do tempo dos bloqueados
-            
-            processo_bloqueado = self.lista_bloqueado[i]
+        for processo_bloqueado in self.lista_bloqueado:
             processo_bloqueado.decrementa_espera()
-            
-            if processo_bloqueado.tempo_de_espera == 0:
-                processos_liberados.append(i)
-                
-            i += 1
-            
-        for i in list(reversed(processos_liberados)):  # liberacão dos processos prontos
-            self.lista_bloqueado.pop(i)
-            self.lista_pronto.insert(len(self.lista_pronto), processo_bloqueado)
-            
-            
-    
+
+        self.lista_bloqueado = [proc for proc in self.lista_bloqueado if proc.tempo_de_espera > 0]
+
     def run(self):
-        while True:
-            
-            processo_atual = self.lista_pronto.pop(0)
-            
-            
-            # ------------ Quantum ------------
-            step = 0
-            n_comp = 0
-            while step < self.quantum:
-                
-                instrucao = processo_atual.instrucoes[processo_atual.program_counter]
-                
-                processo_atual.incrementa_pc()
-                
-                if instrucao == 'E/S':
-                    processo_atual.tempo_de_espera = 5     # hard code para o tempo minimo de espera apos ser bloqueado
-                    
-                    self.lista_bloqueado.insert(len(self.lista_bloqueado), processo_atual)  # insere o processo no fim da fila de block
-                    
-                    break
-                
-                elif instrucao == 'COM':
-                    n_comp += 1
-                    pass
-                
-                elif instrucao == 'SAIDA':
-                    
-                    for proc in self.tabela_de_processos:      # remoção do processo da tabela de processos   
-                                                                                                #####     QUEBRADOOO ############
-                        if tabela_de_processos.is_equal(processo_atual):
-                            tabela_de_processos.pop(proc)
-                        
-                        
-                    break
-                    
-                else:
-                    registrador = instrucao[0]   # registrador alvo
-                    
-                    valor = registrador.split('=')   # valor 
-                    valor = valor[-1]
-                    
-                    if registrador == 'X':
-                        processo_atual.register1 = valor
-                    else:
-                        processo_atual.register2 = valor
-                    
-                    n_comp += 1
-                    
-                
-                step += 1
-                
-            if n_comp == 3:                     # insere novamente o processo na lista de prontos
-                prontos.insert(len(prontos), processo_atual)
+        while self.lista_pronto or self.lista_bloqueado:
+            if self.lista_pronto:
+                processo_atual = self.lista_pronto.pop(0)
+                log(f"Executando {processo_atual.process_name}")
 
-            self.decrementa_espera_bloqueados()
-            
-            
-            
+                # Execução do processo com um quantum limitado
+                for _ in range(self.quantum):
+                    if processo_atual.program_counter < len(processo_atual.instrucoes):
+                        instrucao = processo_atual.instrucoes[processo_atual.program_counter]
+                        processo_atual.incrementa_pc()
 
+                        if instrucao == "E/S":
+                            # Inicia uma operação de E/S e move o processo para a lista de bloqueados
+                            processo_atual.tempo_de_espera = 5
+                            self.lista_bloqueado.append(processo_atual)
+                            log(f"E/S iniciada em {processo_atual.process_name}")
+                            break
+                        elif instrucao == "COM":
+                            # Instrução de cálculo/computação
+                            pass
+                        elif instrucao == "SAIDA":
+                            # Processo concluído; remove-o da lista de prontos e registra os valores dos registradores
+                            self.lista_pronto.remove(processo_atual)
+                            log(f"{processo_atual.process_name} terminado. X={processo_atual.register1}. Y={processo_atual.register2}")
+                            break
+                        else:
+                            registrador, valor = instrucao.split("=")
+                            if registrador == "X":
+                                processo_atual.register1 = int(valor)
+                            else:
+                                processo_atual.register2 = int(valor)
 
-## =========================== Carrega arquivos ===========================================
+                log(f"Interrompendo {processo_atual.process_name} após {self.quantum} instruções")
+                self.decrementa_espera_bloqueados()
+
+# Carrega os processos a partir de arquivos de texto
 def carrega_arquivos():
-    for i in range(11):
-        if i == 0:
-            continue
-        
-        path = 'programas/' + '0' + f'{i}.txt'
-        if i == 10:
-            path = 'programas/' + f'{i}.txt'
-                     
-        
-        with open(path, 'r') as file:
-            codigo_fonte = file.read()
-            instrucoes = codigo_fonte.split('\n')
-            
-        processo = BCP(instrucoes[0], instrucoes[1:-1] , 0, None, None)
-                
-        tabela_de_processos.append(processo)
-        prontos.append(processo)
-        
-    return tabela_de_processos, prontos
-    ## ==================================================================================
+    for i in range(1, 12):
+        nome_arquivo = f"programas/{i:02}.txt"
+        with open(nome_arquivo, "r") as file:
+            linhas = file.readlines()
+            process_name = linhas[0].strip()
+            instrucoes = [linha.strip() for linha in linhas[1:-1]]
+            novo_processo = BCP(process_name, instrucoes, 0, None, None)
+            tabela_de_processos.append(novo_processo)
+            prontos.append(novo_processo)
 
+# Carregando processos a partir dos arquivos
+carrega_arquivos()
 
-tabela_de_processos, prontos = carrega_arquivos()
+# Lendo o valor do quantum do arquivo "quantum.txt"
+with open("quantum.txt", "r") as quantum_file:
+    quantum = int(quantum_file.read())
 
-escalonador = Escalonador(tabela_de_processos, prontos, [])
+# Inicializa escalonador com o valor do quantum e as listas de processos
+escalonador = Escalonador(tabela_de_processos, prontos, bloqueados, quantum)
 
+# Inicie o escalonador
 escalonador.run()
-
-# processo = BCP(1)
-
-# processo.incrementa_pc()
-
-# print(processo.program_counter)
